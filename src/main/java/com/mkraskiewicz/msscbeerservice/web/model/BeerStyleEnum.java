cc85f2aa3efabf92bec08cc5ea5540c376ec5d1a
Maciej Kraskiewicz <maciek.kraskiewicz@gmail.com>
2019-10-19 19:07:24 +0200

    Initial commit

---
+++ src/main/java/com/mkraskiewicz/msscbeerservice/web/model/BeerStyleEnum.java
@@ -0,0 +1,8 @@
+package com.mkraskiewicz.msscbeerservice.web.model;
+
+/**
+ * Created by Maciej on 19/10/2019
+ */
+public enum BeerStyleEnum {
+    LAGER, PILSNER, STOUT, GOSE, PORTER, ALE, WHEAT, IPA, PALE_ALE, SAISON
+}
