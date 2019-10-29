3f0c14b8fdafd4b70e33cca5d1657ea957968a02
Maciej Kraskiewicz <maciek.kraskiewicz@gmail.com>
2019-10-29 18:55:37 +0100

    Added circleCI config

---
+++ src/main/java/com/mkraskiewicz/msscbeerservice/web/mappers/BeerMapper.java
@@ -0,0 +1,16 @@
+package com.mkraskiewicz.msscbeerservice.web.mappers;
+
+import com.mkraskiewicz.msscbeerservice.domain.Beer;
+import com.mkraskiewicz.msscbeerservice.web.model.BeerDto;
+import org.mapstruct.Mapper;
+
+/**
+ * Created by Maciej on 24/10/2019
+ */
+@Mapper(uses = {DateMapper.class})
+public interface BeerMapper {
+
+    BeerDto BeerToBeerDto(Beer beer);
+
+    Beer BeerDtoToBeer(BeerDto dto);
+}
